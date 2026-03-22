68e2cf1f7d49a5dda3ce7a92c72837d5583e6658
Noam30230 <ramillonnoam@gmail.com>
2026-03-22 12:45:03 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+        <title>Account Scorer</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
